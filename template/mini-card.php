c2ab942753f772dff198bd442d98bf2b30df9c5f
Igor3007 <50610898+Igor3007@users.noreply.github.com>
2020-02-26 17:03:15 +0300

    карточка товара, попап

--- template/mini-card.php
+++ template/mini-card.php
@@ -50,7 +50,7 @@
 
                 <!--card-footer-->
                 <div class="t-store__card__btns-wrapper">
-                    <a href="/card.php" class="js-store-prod-btn t-store__card__btn t-btn t-btn_sm">Подробнее</a>
+                    <a href="javascript:;" data-fancybox data-type="ajax" data-src="/card.php" class="js-store-prod-btn t-store__card__btn t-btn t-btn_sm">Подробнее</a>
                     <a href="#order" class="js-store-prod-btn2 t-store__card__btn t-store__card__btn_second t-btn t-btn_sm" >Купить</a>
                 </div>
                 <!--/card-footer-->
